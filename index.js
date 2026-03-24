0fe22f3fb971101bb29dac638623cfeb1e968182
Edson Veiga <edsonsanchezveiga@gmail.com>
2026-03-24 08:33:43 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { ServiceBusClient } = require('@azure/service-bus');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/locacao', async (req, res) => {
+    const { nome, email } = req.body;
+    const connectionString = process.env.SERVICE_BUS_CONNECTION_STRING;
+    
+})
